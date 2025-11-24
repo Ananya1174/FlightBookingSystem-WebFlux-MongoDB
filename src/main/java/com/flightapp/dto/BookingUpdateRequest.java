451390e7be0de62f90d86863ddb66db11706237d
Ananya1174 <157110426+Ananya1174@users.noreply.github.com>
2025-11-25 00:21:32 +0530

    Update BookingUpdateRequest.java

--- src/main/java/com/flightapp/dto/BookingUpdateRequest.java
+++ src/main/java/com/flightapp/dto/BookingUpdateRequest.java
@@ -10,15 +10,11 @@ import lombok.Data;
 @Data
 public class BookingUpdateRequest {
   @Email(message = "Email must be valid") @NotEmpty(message = "Email required") private String email;
-  // optional: user can update their name
   private String name;
 
-  // replace passenger details (optional but when provided should be non-empty)
   private List<Passenger> passengers;
 
-  // optional: request to change seats; if provided, must match passengers size in service
   private List<String> seatNumbers;
 
-  // optional: change meal preference
   private Boolean mealVeg;
 }
